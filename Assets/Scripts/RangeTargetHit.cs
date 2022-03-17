2e42ea8eb9cdf825d91ec1b368a29950ebb00a9b
schau1121 <simon.chau1121@gmail.com>
2022-03-16 21:52:30 -0700

    implemented range side scoring UI

--- Assets/Scripts/RangeTargetHit.cs
+++ Assets/Scripts/RangeTargetHit.cs
@@ -5,18 +5,40 @@ using UnityEngine;
 public class RangeTargetHit : MonoBehaviour
 {
     public ScoringManager m_score = null;
+    private float zone4 = 0.4f;
+    private float zone3 = 0.3f;
+    private float zone2 = 0.2f;
+    private float zone1 = 0.1f;
     //public AudioClip m_hit_sound = null;
     //public AudioSource m_audio = null;
     // Start is called before the first frame update
     void OnCollisionEnter(Collision collision) {
         //Determine distance from center and increment m_score.current_score
         //based off distance
-
+        int hit_score = 0;
         Debug.Log("Range Target Hit");
 
+        Vector3 hit_position = collision.GetContact(0).point;
+        float dist = Vector3.Distance(hit_position, transform.position);
+        Debug.Log("Collision at distance: " + dist);
+
+        if(dist > zone4) {
+            hit_score = 1;
+        }
+        else if(dist > zone3) {
+            hit_score = 2;
+        }
+        else if(dist > zone2) {
+            hit_score = 3;
+        }
+        else if(dist > zone1) {
+            hit_score = 4;
+        }
+        else {
+            hit_score = 5;
+        }
         //m_audio.clip = m_hit_sound;
         //m_audio.Play();
-
-        m_score.current_score++;
+        m_score.current_score = hit_score;
     }
 }
--- Assets/Scripts/ScoringManager.cs
+++ Assets/Scripts/ScoringManager.cs
@@ -1,27 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoringManager : MonoBehaviour
 {
+    public Text scoreboard;
     public int current_score;
     public int tactical_score;
     public int high_score;
+    public Transform camera;
+    private bool in_range;
     // Start is called before the first frame update
     void Start()
     {
         current_score = 0;
         tactical_score = 0;
+
         if(PlayerPrefs.HasKey("HighScore")) {
             high_score = PlayerPrefs.GetInt("HighScore");
         }
+
+        in_range = true;
     }
 
     void Update()
     {
-        if (tactical_score > high_score) {
-            high_score = current_score;
+        if(camera.position[0] <= -9.0f) {
+            in_range = false;
         }
+        else {
+            in_range = true;
+        }
+
+        if (tactical_score > high_score) {
+            high_score = tactical_score;
+        }
+
+        if(in_range) {
+            scoreboard.text = DisplayRangeScore();
+        }
+        else {
+            scoreboard.text = DisplayTacticalScore();
+        }
+
     }
 
     string DisplayRangeScore()
